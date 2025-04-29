11b56ab83242c0304cffc1ee740785bf67a6bc7c
livmrqs <livia.rodrigues3@estudante.ufla.br>
2025-04-29 14:37:28 -0300

    adicionando account e transaction controller

--- src/main/java/uol/compass/hackathon/Banking/System/controller/TransactionController.java
+++ src/main/java/uol/compass/hackathon/Banking/System/controller/TransactionController.java
@@ -1,4 +1,37 @@
 package uol.compass.hackathon.Banking.System.controller;
 
+import uol.compass.hackathon.Banking.System.model.Transaction;
+import uol.compass.hackathon.Banking.System.repository.TransactionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    private final TransactionRepository repository;
+
+    public TransactionController(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Transaction create(@RequestBody Transaction transaction) {
+        return repository.save(transaction);
+    }
+
+    @GetMapping
+    public List<Transaction> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Transaction findById(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
