dce1157fe57c4ec8f64ec6e7c32e7870622b97e6
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2025-04-29 11:37:18 -0300

    Security Config

--- src/main/java/uol/compass/hackathon/Banking/System/config/SecurityConfig.java
+++ src/main/java/uol/compass/hackathon/Banking/System/config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package uol.compass.hackathon.Banking.System.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").permitAll() // libera /api
+                .anyRequest().authenticated() // exige login para o resto
+                .and()
+                .formLogin(); // mantém a tela padrão de login
+        return http.build();
+    }
 }
