9e32221aedde9a792d4e4ef24cfc8d85f554a7d6
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2025-04-30 01:21:30 -0300

    feat: CORS

--- src/main/java/uol/compass/hackathon/Banking/System/controller/ClientController.java
+++ src/main/java/uol/compass/hackathon/Banking/System/controller/ClientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clients")
+@RequestMapping("/api/customers")
 public class ClientController {
     private final ClientRepository repository;
 
